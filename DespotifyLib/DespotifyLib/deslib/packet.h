0f366369f35a2b98c71e1ea31e84bdbb465d0c26
Teemu Sarapisto <tsarpf@gmail.com>
2014-09-28 19:34:47 +0300

    Saving progess.

---
+++ DespotifyLib/DespotifyLib/deslib/packet.h
@@ -0,0 +1,32 @@
+/*
+ * $Id: packet.h 182 2009-03-12 08:21:53Z zagor $
+ *
+ */
+
+#ifndef DESPOTIFY_PACKET_H
+#define DESPOTIFY_PACKET_H
+
+#include "session.h"
+
+/*
+ * Packet header
+ *
+ */
+
+#define PACK( __Declaration__ ) __pragma( pack(push, 1) ) __Declaration__ __pragma( pack(pop) )
+
+PACK(
+struct packet_header
+{
+	unsigned char cmd;
+	unsigned short len;
+};);
+//} __attribute__ ((packed));
+
+
+typedef struct packet_header PHEADER;
+
+/* lowlevel packet functions */
+int packet_read (SESSION * c, PHEADER *, unsigned char **);
+int packet_write (SESSION *, unsigned char, unsigned char *, unsigned short);
+#endif
